8dc2031f0b834d37893f6863ea174dd6e24e0d2d
Kian Kasad <kian@kasad.com>
2025-11-29 23:42:06 -0500

    style: run Prettier

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -3,12 +3,12 @@
  * @type {import("prettier").Config}
  */
 const config = {
-  plugins: ["prettier-plugin-astro", "prettier-plugin-tailwindcss"],
+    plugins: ["prettier-plugin-astro", "prettier-plugin-tailwindcss"],
 
-  // Settings
-  tabWidth: 4,
-  semi: true,
-  singleQuote: false,
+    // Settings
+    tabWidth: 4,
+    semi: true,
+    singleQuote: false,
 };
 
 export default config;
--- eslint.config.js
+++ eslint.config.js
@@ -5,29 +5,29 @@ import eslintPluginAstro from "eslint-plugin-astro";
 import eslintConfigPrettier from "eslint-config-prettier";
 
 export default defineConfig(
-  // Ignore generated files & dependencies
-  {
-    ignores: ["dist/", "node_modules/", ".astro/", ".prettierrc.mjs"],
-  },
-
-  // Base JS
-  eslint.configs.recommended,
-
-  // TypeScript
-  ...tseslint.configs.strictTypeChecked,
-  // Parser settings for type-checked linting
-  {
-    languageOptions: {
-      parserOptions: {
-        projectService: true,
-      },
+    // Ignore generated files & dependencies
+    {
+        ignores: ["dist/", "node_modules/", ".astro/", ".prettierrc.mjs"],
     },
-  },
 
-  // Astro
-  ...eslintPluginAstro.configs.recommended,
-  ...eslintPluginAstro.configs["jsx-a11y-strict"],
+    // Base JS
+    eslint.configs.recommended,
 
-  // Disable rules that conflict with Prettier
-  eslintConfigPrettier,
+    // TypeScript
+    ...tseslint.configs.strictTypeChecked,
+    // Parser settings for type-checked linting
+    {
+        languageOptions: {
+            parserOptions: {
+                projectService: true,
+            },
+        },
+    },
+
+    // Astro
+    ...eslintPluginAstro.configs.recommended,
+    ...eslintPluginAstro.configs["jsx-a11y-strict"],
+
+    // Disable rules that conflict with Prettier
+    eslintConfigPrettier,
 );
