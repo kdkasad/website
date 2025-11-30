871560b52749d17b1f23a1ef6a64f09ef80c070c
Kian Kasad <kian@kasad.com>
2025-11-29 22:47:35 -0500

    chore: configure ESLint

---
+++ eslint.config.js
@@ -0,0 +1,33 @@
+import { defineConfig } from "eslint/config";
+import tseslint from "typescript-eslint";
+import eslint from "@eslint/js";
+import eslintPluginAstro from "eslint-plugin-astro";
+import eslintConfigPrettier from "eslint-config-prettier";
+
+export default defineConfig(
+  // Ignore generated files & dependencies
+  {
+    ignores: ["dist/", "node_modules/", ".astro/"],
+  },
+
+  // Base JS
+  eslint.configs.recommended,
+
+  // TypeScript
+  ...tseslint.configs.strictTypeChecked,
+  // Parser settings for type-checked linting
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+      },
+    },
+  },
+
+  // Astro
+  ...eslintPluginAstro.configs.recommended,
+  ...eslintPluginAstro.configs["jsx-a11y-strict"],
+
+  // Disable rules that conflict with Prettier
+  eslintConfigPrettier,
+);
