642c76a895c9d9d225edf0247b33ef9ea83fe191
Kian Kasad <kian@kasad.com>
2025-12-03 15:44:53 -0500

    refactor: post-process projects during collection loading

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,6 +1,63 @@
-import { defineCollection, z, reference } from "astro:content";
-
+import {
+    defineCollection,
+    z,
+    reference,
+    type SchemaContext,
+} from "astro:content";
 import { glob, file } from "astro/loaders";
+
+import { getBadgesForRepo, type Badge } from "./lib/githubBadges";
+import { SKIP_REMOTE } from "astro:env/server";
+
+const ProjectSchema = ({ image }: SchemaContext) =>
+    z
+        .object({
+            title: z.string(),
+            tagline: z.string(),
+            year: z.coerce.string(),
+            image: z.object({
+                file: image(),
+                caption: z.string(),
+                alt: z.string().optional(),
+            }),
+            githubId: z
+                .string()
+                .regex(
+                    /^[^/]+\/[^/]+$/,
+                    "GitHub repository ID must be <user-or-org>/<repo-name>",
+                )
+                .optional(),
+            links: z
+                .array(
+                    z.object({
+                        name: z.string(),
+                        url: z.string().url(),
+                        icon: z.string(),
+                    }),
+                )
+                .default([]),
+            devicons: z.array(reference("deviconMappings")),
+        })
+        .transform(
+            // Fetches badges from GitHub README and inserts into object, as
+            // well as inserting a link for the GitHub repository.
+            async (project) => {
+                let badges: Badge[] | undefined;
+                if (project.githubId) {
+                    // Insert GitHub repo link
+                    project.links.unshift({
+                        url: `https://github.com/${project.githubId}`,
+                        name: "GitHub repository",
+                        icon: "devicon:github",
+                    });
+                    // Get badges
+                    badges = SKIP_REMOTE
+                        ? []
+                        : await getBadgesForRepo(project.githubId);
+                }
+                return { badges, ...project };
+            },
+        );
 
 export const collections = {
     // This collection is a mapping of devicon IDs to metadata used when
@@ -17,33 +74,6 @@ export const collections = {
 
     projects: defineCollection({
         loader: glob({ pattern: "**/*.md", base: "src/content/projects" }),
-        schema: ({ image }) =>
-            z.object({
-                title: z.string(),
-                tagline: z.string(),
-                year: z.coerce.string(),
-                image: z.object({
-                    file: image(),
-                    caption: z.string(),
-                    alt: z.string().optional(),
-                }),
-                githubId: z
-                    .string()
-                    .regex(
-                        /^[^/]+\/[^/]+$/,
-                        "GitHub repository ID must be <user-or-org>/<repo-name>",
-                    )
-                    .optional(),
-                links: z
-                    .array(
-                        z.object({
-                            name: z.string(),
-                            url: z.string().url(),
-                            icon: z.string(),
-                        }),
-                    )
-                    .default([]),
-                devicons: z.array(reference("deviconMappings")),
-            }),
+        schema: ProjectSchema,
     }),
 };
