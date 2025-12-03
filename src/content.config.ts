f87dbb7b4437a5f2b171c2ea521254c1e29a80e7
Kian Kasad <kian@kasad.com>
2025-12-02 19:48:20 -0500

    feat(landing): add projects section

---
+++ src/content.config.ts
@@ -0,0 +1,48 @@
+import { defineCollection, z, reference } from "astro:content";
+
+import { glob, file } from "astro/loaders";
+
+export const collections = {
+    // This collection is a mapping of devicon IDs to metadata used when
+    // displaying them, such as a full name for what the icon represents and a
+    // URL for more information about the technology
+    deviconMappings: defineCollection({
+        loader: file("src/content/devicons.yml"),
+        schema: z.object({
+            url: z.string().url().optional(),
+            name: z.string().nonempty(),
+        }),
+    }),
+
+    projects: defineCollection({
+        loader: glob({ pattern: "**/*.md", base: "src/content/projects" }),
+        schema: ({ image }) =>
+            z.object({
+                title: z.string(),
+                tagline: z.string(),
+                year: z.coerce.string(),
+                image: z.object({
+                    file: image(),
+                    caption: z.string(),
+                    alt: z.string().optional(),
+                }),
+                githubId: z
+                    .string()
+                    .regex(
+                        /^[^/]+\/[^/]+$/,
+                        "GitHub repository ID must be <user-or-org>/<repo-name>",
+                    )
+                    .optional(),
+                links: z
+                    .array(
+                        z.object({
+                            name: z.string(),
+                            url: z.string().url(),
+                            icon: z.string(),
+                        }),
+                    )
+                    .default([]),
+                devicons: z.array(reference("deviconMappings")),
+            }),
+    }),
+};
---
+++ src/lib/githubBadges.ts
@@ -0,0 +1,65 @@
+/**
+ * Helper utilities to extract badges from the README files of GitHub projects.
+ */
+
+import { GH_API_TOKEN } from "astro:env/server";
+import z from "zod";
+
+export interface Badge {
+    linkUrl: string;
+    imageUrl: string;
+    alt: string;
+}
+
+const Badge = z.object({
+    linkUrl: z.string(),
+    imageUrl: z.string(),
+    alt: z.string(),
+});
+
+const FileResponse = z.object({
+    content: z.string(),
+    html_url: z.string().url(),
+});
+
+async function getGitHubFile(
+    repoId: string,
+    file: string,
+): Promise<z.infer<typeof FileResponse>> {
+    const url = `https://api.github.com/repos/${repoId}/contents/${file}`;
+    const response = await fetch(url, {
+        headers: {
+            Authorization: `Bearer ${GH_API_TOKEN}`,
+            Accept: "application/vnd.github.object+json",
+        },
+        redirect: "follow",
+    });
+    if (!response.ok) {
+        throw new Error(
+            `GitHub API returned ${response.status} ${response.statusText} for ${url}`,
+        );
+    }
+    return FileResponse.parse(await response.json());
+}
+
+export async function getBadgesForRepo(repoId: string): Promise<Badge[]> {
+    const fileInfo = await getGitHubFile(repoId, "README.md");
+    const markdown = atob(fileInfo.content);
+
+    // Extract badges from Markdown content
+    const matches = markdown.matchAll(
+        /\[!\[(?<alt>.*?\bbadge\b.*?)\]\((?<imageUrl>.*?)\)\]\((?<linkUrl>.*?)\)/g,
+    );
+    const badges = Array.from(matches, (match) => Badge.parse(match.groups));
+
+    // Convert relative links to absolute ones
+    for (const badge of badges) {
+        if (badge.linkUrl.match(/https?:\/\//) === null) {
+            // URL is relative; get absolute URL from GitHub API
+            const fileInfo = await getGitHubFile(repoId, badge.linkUrl);
+            badge.linkUrl = fileInfo.html_url;
+        }
+    }
+
+    return badges;
+}
